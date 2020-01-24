0404f69d98bd34cfaf00dee4800fc2d5764583f7
Junya Iizuka <minazukitombo@gmail.com>
2020-01-25 00:58:42 +0900

    Updates

---
+++ precache-manifest.2c2f8c3af7419a3016334e1162271787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289ee30bfb3c1083d6229268f37e507c",
+    "url": "/react-faq-tool/index.html"
+  },
+  {
+    "revision": "b9b4c6a86d6ca167d87e",
+    "url": "/react-faq-tool/static/css/main.b5ffcf6a.chunk.css"
+  },
+  {
+    "revision": "64e993d0edaf698f5ff0",
+    "url": "/react-faq-tool/static/js/2.76197350.chunk.js"
+  },
+  {
+    "revision": "764ee7d7b2b37ea6ad2fc352f6f4060b",
+    "url": "/react-faq-tool/static/js/2.76197350.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9b4c6a86d6ca167d87e",
+    "url": "/react-faq-tool/static/js/main.aadc3671.chunk.js"
+  },
+  {
+    "revision": "de2789721e5a7dcee9e5",
+    "url": "/react-faq-tool/static/js/runtime-main.539c3804.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53b605f69524c6f2e30523d83712f434.js"
+  "/react-faq-tool/precache-manifest.2c2f8c3af7419a3016334e1162271787.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-faq-tool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
